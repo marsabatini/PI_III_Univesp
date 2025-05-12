ef47933d32bafbf944735bcf0b64fd926aae82a6
LeoSilvaz <leo.slsilvaz@gmail.com>
2025-05-12 05:52:13 -0300

    mudanças tela presença

--- 03_Front-End/academiateamcrezio/src/Pages/Home/Presenca/index.js
+++ 03_Front-End/academiateamcrezio/src/Pages/Home/Presenca/index.js
@@ -9,11 +9,11 @@ import Header from "../../Components/Header";
 
 
 export default function Presenca() {
-    
+
     const [presencaConfirmada, setPresencaConfirmada] = useState(false);
 
     const confirmarPresenca = () => {
-        setPresencaConfirmada(true); 
+        setPresencaConfirmada(true);
     };
 
     return (
@@ -24,22 +24,173 @@ export default function Presenca() {
                     <article className="main_bnt">
                         <section>
                             <h1>
-                                aperte o botão abaixo para confirmar sua presença 
+                                veja suas aulas e confirme sua presença
                             </h1>
                         </section>
-                        <section className="sec_bnt">
-                            <button id="btn" onClick={confirmarPresenca} className="presenca_bnt">
-                                clique aqui
-                            </button>
 
-                            { }
-                            {presencaConfirmada && (
-                                <div className="msg_bnt">
-                                    presença confirmada!
-                                </div>
-                            )}
+                        <section className="tabela_aulas">
 
+                            <div className="presencablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                                <button className="confirmar-btn" >
+                                    Confirmar Presença
+                                </button>
+                            </div>
+
+                            <div className="presencablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                                <button class="confirmar-btn" >
+                                    Confirmar Presença
+                                </button>
+                            </div>
+
+                            <div className="presencablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                                <button class="confirmar-btn">
+                                    Confirmar Presença
+                                </button>
+                            </div>
+
+                            <div className="presencablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                                <button class="confirmar-btn" >
+                                    Confirmar Presença
+                                </button>
+                            </div>                           
                         </section>
+
+                        <section>
+                            <h1>
+                                veja suas proximas aulas
+                            </h1>
+                        </section>
+                        <section className="tabela_aulas ">
+                        
+                        <div className="aulablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                        </div>
+
+                        <div className="aulablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                        </div>
+
+                        <div className="aulablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                        </div>
+
+                        <div className="aulablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                        </div>
+
+                        <div className="aulablocos">
+                                <h3>
+                                    boxe
+                                </h3>
+                                <span id="data_aula">
+                                    20/04/25
+                                </span>
+                                <span id="horario_aula">
+                                    20:00
+                                </span>
+                                <span id="dia_aula">
+                                    quarta-feira
+                                </span>
+                        </div>
+                      </section>
+                      <section className="espaco">
+                     </section>
                     </article>
                 </main>
                 <Footer />
